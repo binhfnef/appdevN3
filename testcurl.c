477dbcad478ec803f48578d8d4082b8dafed6ae2
binhfnef <56982687+binhfnef@users.noreply.github.com>
2020-05-04 17:40:01 +0300

    Update testcurl.c

--- testcurl.c
+++ testcurl.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <i386-linux-gnu/curl/curl.h>
 
-#define URL "http://www.cc.puv.fi/~e1901100/php/appdevN3.php"
+#define URL "http://www.cc.puv.fi/~gc/php/raspsound.php"
 
-
-int main(void){
+int main(void)
+{
 	CURL *curl;
 	CURLcode res;
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
-	curl = curl_easy_init();
+	curl=curl_easy_init();
 
-	if (curl){
-		if(argc<2)
-			curl_easy_setopt(curl, CURLOPT_URL, URL);
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=Binh_Nguyen&ID=e1901100 ");
-		res = curl_easy_perform(curl);
+	if(curl)
+	{
+		curl_easy_setopt(curl, CURLOPT_URL, URL);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=Binh_Nguyen&ID=e1901100");
+		res=curl_easy_perform(curl);
 		if(res != CURLE_OK)
 			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
 		curl_easy_cleanup(curl);
-	 }
+	}	//end of if
 	curl_global_cleanup();
 	return 0;
 }
+
