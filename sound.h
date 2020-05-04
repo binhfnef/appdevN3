44ddd2a714162ba054de87ea537dd121a341263a
binhfnef <56982687+binhfnef@users.noreply.github.com>
2020-05-04 17:36:39 +0300

    Update sound.h

--- sound.h
+++ sound.h
@@ -1,27 +1,25 @@
-// in this file we define related constants and declare functions
-// for handling sound.
-#define SDEBUG
+// This header file contains constant definitions and function declarations
+// for processing sound, specifically .wav files.
+//#define SDEBUG
 
-// define sound structure (deal with .wav file)
-
-typedef struct{
-	char chunkID[4];		// should be always "RIFF"
-	int chunkSize;
-	char format[4];			// should be always "WAVE"
-	char subchunk1ID[4]; 	// should be always "fmt "
-	int subchunk1Size; 		// should be 16 for PCM
-	short audioFormat;		// should be 1 for PCM
-	short numChannels;		// could be 1 for mono, 2 for stereo
-	int sampleRate;			// usually it's 44100, or 16000
-	int byteRate;			// can be calculated
-	short blockAlign;		// how many bytes in one block
-	short bitsPerSample;	// could be 8 or 16
-	char subchunk2ID[4];	// should be always "data"
-	int subchunk2Size;		// can be calculated
+typedef struct
+{
+	char chunkID[4];	// should be always "RIFF"
+	int chunkSize;          // indicates how big sound data is
+	char format[4];  	// should be always "WAVE"
+	char subchunk1ID[4];	// should be always "fmt "
+	int subchunk1Size;	// should be 16 for PCM data
+	short audioFormat;	// should be 1 for Linear samples
+	short numChannels;	// 1 for mono, 2 for stereo
+	int sampleRate;    	// could be 44100, 16000, 8000
+	int byteRate;		// can be calculated
+	short blockAlign;	// how many bytes in one block
+	short bitsPerSample;	// how many bits in one sample
+	char subchunk2ID[4];	//should be always "data"
+	int subchunk2Size;	// how many bytes exactly for data part
 }WAVheader;
 
-
-// function declaration
-WAVheader readwavhdr(FILE *fp);
-void displayWAVhdr(WAVheader h);
+// function declarations
+WAVheader readwavhdr(FILE *);
+void displaywavhdr (WAVheader);
 void wavdata(WAVheader h, FILE *fp);
