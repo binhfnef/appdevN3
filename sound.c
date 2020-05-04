bd8cc2ad935a83710a2a8cc46403cab433dec861
binhfnef <56982687+binhfnef@users.noreply.github.com>
2020-05-04 19:24:50 +0300

    Update sound.c

--- sound.c
+++ sound.c
@@ -1,9 +1,9 @@
+  
 #include <stdio.h>
 #include "sound.h"
 #include <math.h>
-#include "comm.h"
 #include "screen.h"
-
+#include "comm.h"
 // function definitions
 WAVheader readwavhdr(FILE *fp){
 	WAVheader myh;
@@ -11,64 +11,71 @@ WAVheader readwavhdr(FILE *fp){
 	return myh;
 }
 
-void displaywavhdr(WAVheader h){
-	printf("Chunk ID: ");
-	for(int i=0; i<4; i++)	printf("%c", h.chunkID[i]);
+void displaywavhdr (WAVheader h){
+	for(int i=0; i<4; i++)
+		printf("%C", h.chunkID[i]);
 	printf("\n");
 	printf("Chunk size: %d\n", h.chunkSize);
-	printf("Number of channels: %d\n", h.numChannels);
+	printf("Number of Channels: %d\n", h.numChannels);
 	printf("Sample rate: %d\n", h.sampleRate);
-	printf("Block align: %d\n", h.blockAlign);
 	printf("Bits per sample: %d\n", h.bitsPerSample);
 	// -- to be continued
+	double duration;
+	duration =(double) h.subchunk2Size/h.byteRate;
+	printf("Duration: %f seconds\n", duration);
 }
 
 void wavdata(WAVheader h, FILE *fp){
-	int peaks=0, flag=0;	// 1st value is to count, 2nd value to show that you are in a peak
-	// in this function we will read sound samples from the opened file
-	// the samples are calculated in to decibel value using Root Mean Square
-	// (RMS) formula. We will display a 5-sec recorded sound into bar chart
-	// our sound file uses sample rate of 16000, for 5 seconds, there are
-	// 5*16000 = 800000 samples, we want to display them into 160 bars
-	short samples[500];	// to read 500 samples from wav file
-	double max = 0.0;
-	for(int i=0; i<160; i++){
+	// for sample rate 16000sps, we need to read 2000 samples to calculate a
+	// "Fast" decibel calue. A decibel value is always calculated by RMS
+	// (ROOT MEAN SQUARE) formula.
+	int peaks = 0, flag = 0; 
+	double max=0.0;
+	char postdata[100];
+	short samples[SIZE];
+	for(int i=0; i<BARS; i++){	//to read 5-sec wave file, we have 40 data
 		fread(samples, sizeof(samples), 1, fp);
-		double sum = 0.0;	//accumulate the sum
-		for(int j=0; j<500; j++){
-			sum = sum + samples[j]*samples[j];
+		double sum = 0.0;
+		for(int k=0; k<SIZE; k++){	// sum the squares of all data
+			sum = sum + samples[k]*samples[k];
 		}
-		double re = sqrt(sum/500);
+		//double dB = sqrt(sum/2000);
+		double dB = 20*log10(sqrt(sum/SIZE));
 #ifdef SDEBUG
-	printf("db[%d] = %f\n", i+1, 20*log10(re));
+		printf("dB[%d] = %f\n", i, dB);
 #else
-	// displaybar for re value
-	if(20*log10(re)>60){
-		setfgcolor(RED);
-		if(flag == 0){
-			flag = 1;
-			peaks++;	// I got a peak
+		if(dB>70){
+			 setfgcolor(RED);
+			if (flag==0){
+				peaks++;
+				flag=1;
+			}
 		}
-	}
-	else{
-		setfgcolor(WHITE);
-		flag = 0;
-	}
-	drawbar(i+1, (int)20*log10(re)/3);
-
+		else{
+			setfgcolor(WHITE);
+			if (flag==1);
+				flag=0;
+		}
+		if(max<1.0)
+			max=dB;
+		else{
+			if(dB>max)
+				max=dB;
+		}
+		drawbar(i+1,(int) dB/3);
+		gotoXY(1,1);
+		setfgcolor(CYAN);
+		printf("Sample rate: %d\n", SAMPLERATE);
+		gotoXY(1,75);
+		setfgcolor(MAGENTA);
+		printf("Duration: %.2f s\n",(double) h.subchunk2Size/h.byteRate);
+		gotoXY(1,150);
+		setfgcolor(YELLOW);
+		printf("Peaks: %d\n", peaks);
+		gotoXY(2,1);
+		printf("Max: %lf\n", max);	
 #endif
-	if(20*log10(re)>max){
-		max = 20*log10(re);
 	}
-	}
-	// display sample rate, duration, no. of peaks on top of the screen
-	gotoXY(1, 1);	printf("Sample Rate: %d\n", h.sampleRate);
-	gotoXY(1, 75);	printf("Duration: %f s\n", (float)h.subchunk2Size/h.byteRate);
-	gotoXY(1, 150); printf("Peaks: %d\n", peaks);
-	//Position cur = getscreensize(); //get screen size
-        char postdata[100];
-        sprintf(postdata, "peaks=%d&max=%f\n", peaks, max);
-        sendpost(URL, postdata);
-
+	sprintf(postdata, "Peaks=%d&MaxdB=%lf", peaks, max);
+	sendpost(URL, postdata);
 }
-// end of file
